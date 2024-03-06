92fa0f6aa8806d1a9f6abe1e81bfa495237df562
anabrandt <anactavaresbrand@gmail.com>
2024-03-06 11:47:38 -0300

    Atributos

--- src/main/java/Veiculo.java
+++ src/main/java/Veiculo.java
@@ -1,12 +1,36 @@
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+package br.com.benefrancis.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
+@Table(name = "TB_SABOR")
 public class Veiculo {
-    Id long;
-    String nome;
-    String cor;
-    Double preco;
-    Short cilindradas;
-}
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_SABOR")
+    @SequenceGenerator(
+            name = "SQ_SABOR",
+            sequenceName = "SQ_SABOR",
+            initialValue = 1,
+            allocationSize = 1
+    )
+    @Column(name = "ID_SABOR")
+    private Long id;
+
+
+    @Column(name = "NM_SABOR")
+    private String nome;
+
+    @Column(name = "DS_SABOR")
+    private String descricao;
+
+
+}
